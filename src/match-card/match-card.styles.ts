546a02219e201fd4d2074c8cd7cae2ae6f49097f
Khagesh Kumar <khagesh.kumar@dream11.com>
2022-03-12 15:58:07 +0530

    feat: card content with flag

--- src/match-card/match-card.styles.ts
+++ src/match-card/match-card.styles.ts
@@ -7,6 +7,8 @@ const colors = {
   PRIMARY: '#1a1a1a',
   SECONDARY: '#797979',
   TERNARY: '#109E38',
+
+  RED: '#e10000',
 };
 
 const BORDER_RADIUS = 8;
@@ -58,6 +60,71 @@ export const styles = StyleSheet.create({
     height: 1,
     borderRadius: 1 / 2,
     marginHorizontal: 12,
+  },
+  /** ------------------------------------ Title ------------------------------------ */
+  tContainer: {
+    paddingHorizontal: 12,
+    marginTop: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  tText: {
+    fontSize: 12,
+    lineHeight: 16,
+    fontWeight: '400',
+    color: colors.PRIMARY,
+  },
+  /** ------------------------------------ Main ------------------------------------ */
+  mContainer: {
+    paddingHorizontal: 9,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 1,
+    marginBottom: 5,
+  },
+  mLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  mCenter: {
+    fontSize: 12,
+    lineHeight: 16,
+    fontWeight: '700',
+    color: colors.RED,
+  },
+  mRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  mFlagIcon: {
+    width: 31,
+    height: 31,
+    margin: 3,
+  },
+  mFlagSHortName: {
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: '700',
+    color: colors.PRIMARY,
+    marginHorizontal: 4,
+  },
+  mLeftGradient: {
+    height: 20,
+    width: 24,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    position: 'absolute',
+    left: 0,
+  },
+  mRightGradient: {
+    height: 20,
+    width: 24,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    position: 'absolute',
+    right: 0,
   },
   /** ------------------------------------ Footer ------------------------------------ */
   fContainer: {
--- src/match-card/match-card.tsx
+++ src/match-card/match-card.tsx
@@ -18,8 +18,37 @@ export const MatchCard = () => {
           style={styles.hIcon}
         />
       </View>
+      {/* ------------------------------------ divider ------------------------------------*/}
       <View style={styles.divider} />
-      <Text>Match Card</Text>
+      {/* ------------------------------------ Title ------------------------------------*/}
+      <View style={styles.tContainer}>
+        <Text style={styles.tText}>Dortmund</Text>
+        <Text style={styles.tText}>Bayren</Text>
+      </View>
+      {/* ------------------------------------ Main ------------------------------------*/}
+      <View style={styles.mContainer}>
+        <View style={[styles.mLeftGradient, {backgroundColor: '#afafff'}]} />
+        <View style={[styles.mRightGradient, {backgroundColor: '#b6d639'}]} />
+        <View style={styles.mLeft}>
+          <Image
+            source={{
+              uri: 'https://d13ir53smqqeyp.cloudfront.net/flags/cr-flags/TW-CR1@2x-BIG.png',
+            }}
+            style={styles.mFlagIcon}
+          />
+          <Text style={styles.mFlagSHortName}>DOR</Text>
+        </View>
+        <Text style={styles.mCenter}>1h 22m</Text>
+        <View style={styles.mRight}>
+          <Text style={styles.mFlagSHortName}>BAY</Text>
+          <Image
+            source={{
+              uri: 'https://d13ir53smqqeyp.cloudfront.net/flags/cr-flags/GS-CR1@2x-BIG.png',
+            }}
+            style={styles.mFlagIcon}
+          />
+        </View>
+      </View>
       {/* ------------------------------------ Footer ------------------------------------ */}
       <View style={styles.fContainer}>
         <View style={styles.fLeft}>
